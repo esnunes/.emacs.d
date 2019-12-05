b4a1569c43ae2a563b727beb1e9b1df6b1617bd3
Eduardo Nunes <esnunes@gmail.com>
2019-12-05 13:47:16 +0100

    Prevent font change when displaying code in markdown

--- my/my-markdown.el
+++ my/my-markdown.el
@@ -2,7 +2,11 @@
   :ensure t
   :mode (("\\.md\\'" . gfm-mode)
          ("\\.markdown\\'" . gfm-mode))
+  :init
+  ;; enable auto-fill
+  ;; (add-hook 'markdown-mode-hook 'auto-fill-mode)
   :config
+  (set-face-attribute 'markdown-code-face nil :inherit nil)
   (setq markdown-fontify-code-blocks-natively t))
-  
+
 (provide 'my-markdown)
