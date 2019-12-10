7ddb4d64e4fe4fd3d460b6ddf1ccb7fe9f62141e
Eduardo Nunes <esnunes@gmail.com>
2019-12-10 13:28:07 +0100

    Set default window size

--- my/my-ui.el
+++ my/my-ui.el
@@ -59,4 +59,10 @@
 (add-to-list 'default-frame-alist
 	     '(font . "Inconsolata-14"))
 
+;; set default GUI window size
+(add-to-list 'default-frame-alist
+	     '(width . 162))
+(add-to-list 'default-frame-alist
+	     '(height . 45))
+
 (provide 'my-ui)
