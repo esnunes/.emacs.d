71c3ae2f10b19a8e3377315b59bb57f68e285315
Eduardo Nunes <esnunes@gmail.com>
2020-01-25 02:07:04 +0100

    Add company autocomplete package

--- my/my-core.el
+++ my/my-core.el
@@ -42,6 +42,20 @@
   :config
   (global-set-key (kbd "C-s") 'swiper))
 
+(use-package company
+  :ensure t
+  :config
+  (setq company-idle-delay 0.5)
+  (setq company-show-numbers t)
+  (setq company-tooltip-limit 10)
+  (setq company-minimum-prefix-length 2)
+  (setq company-tooltip-align-annotations t)
+  ;; invert the navigation direction if the the completion popup-isearch-match
+  ;; is displayed on top (happens near the bottom of windows)
+  (setq company-tooltip-flip-when-above t)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
+
 (use-package counsel
   :ensure t
   :config
