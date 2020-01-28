9e112c4baed0d8a879f705826813943dcab0931f
Eduardo Nunes <esnunes@gmail.com>
2020-01-28 20:25:15 +0100

    Bind Meta+Tab to force auto complete

--- my/my-core.el
+++ my/my-core.el
@@ -58,6 +58,7 @@
   ;; invert the navigation direction if the the completion popup-isearch-match
   ;; is displayed on top (happens near the bottom of windows)
   (setq company-tooltip-flip-when-above t)
+  (global-set-key (kbd "C-M-i") #'company-complete)
   (define-key company-active-map (kbd "C-n") #'company-select-next)
   (define-key company-active-map (kbd "C-p") #'company-select-previous))
 
