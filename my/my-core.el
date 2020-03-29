76e695d8ba0333aaf71ec7ff722d87a0486c3b94
Eduardo Nunes <esnunes@gmail.com>
2020-03-29 16:58:36 +0200

    Do not rely on keys not available on terminal mode

--- my/my-core.el
+++ my/my-core.el
@@ -26,7 +26,7 @@
   :init
   (setq projectile-completion-system 'ivy)
   :config
-  (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+  (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
   (projectile-mode +1))
 
 (use-package ivy
